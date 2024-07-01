8a7a78c9228440d2caf6764b169d0fb8d0db6db9
Gamal Soliman <gsoliman70@gmail.com>
2024-07-01 13:28:35 +0300

    plus if condition

--- addtwonumbers.dart
+++ addtwonumbers.dart
@@ -7,6 +7,14 @@ addtwonumbers(44,55);
 addtwonumbers(476,6);
 addtwonumbers(423,576);
 addtwonumbers(434,567);
+int x=25;
+int y=23;
+if (x>y) {
+print('the first number is larger than the second number');  
+}else{
+  print('the second number is larger than the first number');
+
+}
 }
 void addtwonumbers( int number1 , int number2){
 print(number1+number2);
