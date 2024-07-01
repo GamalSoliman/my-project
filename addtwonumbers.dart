ec0ff08d78708d66dfa6fe77339fc4d29e7797d8
Gamal Soliman <gsoliman70@gmail.com>
2024-07-01 13:07:39 +0300

    intialising the function

---
+++ addtwonumbers.dart
@@ -0,0 +1,13 @@
+
+void main(){
+addtwonumbers(2,3);
+addtwonumbers(4,5);
+addtwonumbers(3,5);
+addtwonumbers(44,55);
+addtwonumbers(476,6);
+addtwonumbers(423,576);
+addtwonumbers(434,567);
+}
+void addtwonumbers( int number1 , int number2){
+print(number1+number2);
+}
