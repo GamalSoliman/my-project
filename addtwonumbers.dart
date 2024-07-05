cb2ede3d8387b9a8986ed24c0d4ab8eedefbf8b3
Gamal <gsoliman70@gmail.com>
2024-07-05 20:19:45 +0300

    changing function

--- addtwonumbers.dart
+++ addtwonumbers.dart
@@ -14,11 +14,15 @@ print('the first number is larger than the second number');
 }else{
   print('the second number is larger than the first number');
 }*/
-var m=50;
+/*var m=50;
 var n=40;
 String result= (n>m) ? 'n is larger than m ': 'm is larger than n';
 print(result);
+*/
+int result = addtwonumbers(5,10);
+print(result);
 }
-/*void addtwonumbers( int number1 , int number2){
-print(number1+number2);
-}*/
+int addtwonumbers( int number1 , int number2 ,[int m=0 , int n=0]){
+int z = number1+number2 + m + n;
+return(z);
+}
